a10856bb017b23e05691ebfbdffb676e41512c1d
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-09-12 14:19:13 +0600

    Wrote the logic of generating random numbers, created a button 'New numbers'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
 import Ball from './components/Ball/Ball.tsx';
+import React, { useState } from 'react';
 
-const numbers = [5, 11, 16, 23, 32];
+const generateNewNumbers = () => {
+    const numbers = new Set<number>();
+    while (numbers.size < 5) {
+        numbers.add(Math.floor(Math.random() * 32) + 5);
+    }
+    return Array.from(numbers).sort((a, b) => a - b);
+}
+
 
 const App = () => {
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const getRandomNumbers = () => {
+        setNumbers(generateNewNumbers());
+    }
+
     return (
         <div className='container'>
         <div className= 'balls'>
@@ -11,6 +25,7 @@ const App = () => {
                 <Ball key={index} number={number} />
             ))}
         </div>
+            <button className= 'button' onClick={getRandomNumbers} type='button'>New numbers</button>
         </div>
     );
 };
